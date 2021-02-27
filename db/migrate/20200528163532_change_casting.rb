05928c440b57d1fae5f85c0bfddd83930feac65e
jakepino <jakepino@yahoo.com>
2021-02-27 14:20:23 -0500

    transfer

---
+++ db/migrate/20200528163532_change_casting.rb
@@ -0,0 +1,5 @@
+class ChangeCasting < ActiveRecord::Migration[6.0]
+  def change
+    change_column :resources, :casting_type, :json
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_154547) do
+ActiveRecord::Schema.define(version: 2020_05_28_163532) do
+
+  create_table "casting_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
